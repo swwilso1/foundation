a1b058b3c43ef0242b13dc7401c8e78a2de9978d
Steve Wilson <swwilso1@protonmail.com>
2024-03-07 13:49:32 -0700

    Add constants module.

---
+++ src/constants.rs
@@ -0,0 +1,17 @@
+pub const KILO: u64 = 1024;
+pub const MEGA: u64 = 1024 * KILO;
+pub const GIGA: u64 = 1024 * MEGA;
+pub const TERA: u64 = 1024 * GIGA;
+pub const PETA: u64 = 1024 * TERA;
+pub const EXA: u64 = 1024 * PETA;
+pub const ZETTA: u128 = 1024 * (EXA as u128);
+pub const YOTTA: u128 = 1024 * ZETTA;
+
+pub const MKILO: u64 = 1000;
+pub const MMEGA: u64 = 1000 * MKILO;
+pub const MGIGA: u64 = 1000 * MMEGA;
+pub const MTERA: u64 = 1000 * MGIGA;
+pub const MPETA: u64 = 1000 * MTERA;
+pub const MEXA: u64 = 1000 * MPETA;
+pub const MZETTA: u128 = 1000 * (MEXA as u128);
+pub const MYOTTA: u128 = 1000 * MZETTA;
