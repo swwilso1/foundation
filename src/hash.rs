fd2b9f31b60e4bf3c126576096e9c4e8d11c5604
Steve Wilson <swwilso1@protonmail.com>
2024-10-14 09:01:03 -0600

    Restore hash.rs and progressmeter.rs from 6947d9a.

--- src/hash.rs
+++ src/hash.rs
@@ -78,7 +78,7 @@ pub async fn async_get_hash_for_file_with_meter(
         hasher.update(&chunk[..bytes_read]);
         if let Ok(mut meter) = meter.lock() {
             meter.increment_by(bytes_read as u64);
-            meter.notify(false).await;
+            meter.notify(false);
         }
     }
 
@@ -186,7 +186,7 @@ pub async fn async_get_hash_for_dir_with_meter(
                 hasher.update(&chunk[..bytes_read]);
                 if let Ok(mut meter) = meter.lock() {
                     meter.increment_by(bytes_read as u64);
-                    meter.notify(false).await;
+                    meter.notify(false);
                 }
             }
             if include_file_names {
