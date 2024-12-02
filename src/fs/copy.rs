9802ff34aefafc56fd96c93a780593b1355fd7c1
Steve Wilson <swwilso1@protonmail.com>
2024-12-02 13:11:08 -0700

    Update the synchronous copy code to not call the asynchronous version.

--- src/fs/copy.rs
+++ src/fs/copy.rs
@@ -1,8 +1,12 @@
 use crate::error::FoundationError;
-use crate::fs::async_copy;
 use crate::progressmeter::ProgressMeter;
+use nix::unistd::fsync;
+use std::io::{Read, Write};
+use std::os::fd::AsRawFd;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
+
+const BLOCKSIZE: libc::size_t = 8388608;
 
 /// Synchronously copy a file from one location to another.
 ///
@@ -22,7 +26,53 @@ pub fn copy(
     dest: &Path,
     meter: Option<Arc<Mutex<ProgressMeter>>>,
 ) -> Result<(), FoundationError> {
-    tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(async { async_copy(src, dest, meter).await })
+    if !src.exists() {
+        return Err(FoundationError::FileNotFound(src.to_path_buf()));
+    }
+
+    // Get the number of bytes in the source file.
+    let mut src_bytes = std::fs::metadata(src)?.len();
+
+    // Create the destination file.
+    let mut dest_file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(dest)?;
+
+    // Get the destination file descriptor. We use this to call fsync to make sure
+    // the data is written to disk.
+    let dest_fd = dest_file.as_raw_fd();
+
+    let mut src_file = std::fs::File::open(src)?;
+
+    while src_bytes > 0 {
+        let mut buffer = vec![0u8; BLOCKSIZE];
+        let bytes_read = src_file.read(&mut buffer)?;
+        if bytes_read == 0 && src_bytes > 0 {
+            continue;
+        }
+
+        dest_file.write_all(&buffer[..bytes_read])?;
+        dest_file.flush()?;
+
+        if let Some(meter) = &meter {
+            if let Ok(mut meter) = meter.lock() {
+                meter.increment_by(bytes_read as u64);
+                meter.notify(false);
+            }
+        }
+
+        src_bytes -= bytes_read as u64;
+    }
+
+    // Make sure to sync the writes to the destination.
+    if let Err(e) = fsync(dest_fd) {
+        return Err(FoundationError::SyncError(format!(
+            "Failed to sync data: {}",
+            e
+        )));
+    }
+
+    Ok(())
 }
